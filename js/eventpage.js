4b5a9427d8cfcfd0a48029a9d9254a28a04410f2
jose <joseb@ensemble.com>
2016-09-19 22:21:41 +0100

    Adds Open Sans CDN to the page header. Removes local font files.

--- js/eventpage.js
+++ js/eventpage.js
@@ -76,6 +76,7 @@ window.onresize = function() {
 // ******************************************************************
 
 $(document).ready(function() {
+    $('head').append('<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />');
     $(".tabs-menu a").click(function(event) {
         event.preventDefault();
         $(this).parent().addClass("current");
