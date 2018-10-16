75147951282186447de7ce3bea844040fd20cf30
Jose <jose.berciano@cognifide.com>
2018-10-16 08:22:05 +0200

    Gulp, building dist files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import autoprefixer from 'gulp-autoprefixer';
 import sourcemaps from 'gulp-sourcemaps';
 import runSequence from 'run-sequence';
 import concat from 'gulp-concat';
+import gulpCopy from 'gulp-copy';
+
 
 const dirs = {
   src: 'src',
@@ -12,7 +14,7 @@ const dirs = {
 
 const sassPaths = {
   src: `${dirs.src}/scss/**/*.scss`,
-  dest: `${dirs.dest}/styles/`
+  dest: `${dirs.dest}/css/`
 };
 
 const jsPaths = {
@@ -21,12 +23,12 @@ const jsPaths = {
 };
 
 
-gulp.task('build-js', function () {
+gulp.task('js', function () {
     return gulp.src(['./src/js/lib/jquery.js',
                     './src/js/lib/axios.js',
                      './src/js/eventpage.js'])
             .pipe(sourcemaps.init())
-            .pipe(concat('bundle.js'))
+            .pipe(concat('eventpage.js'))
             .pipe(sourcemaps.write('.'))
             .pipe(gulp.dest(jsPaths.dest));
 });
@@ -40,10 +42,32 @@ gulp.task('sass', () => {
     .pipe(gulp.dest(sassPaths.dest));
 });
 
+gulp.task('copy-assets', function () {
+    gulp.src('./src/assets/*')
+        .pipe(gulp.dest('./build/assets'));
+});
 
+gulp.task('copy-files', function () {
+    gulp.src(['./src/popup.html', './src/manifest.json', './src/chrome-extension.iml'])
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch:css', function () {
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch:js', function () {
+    gulp.watch('./src/js/**/*.js', ['js']);
+});
+
+gulp.task('watch', function(){
+    runSequence(
+      'watch:css', 'watch:js'
+    );
+});
 
 gulp.task('default', function(){
     runSequence(
-      'build-js','sass'
+      'js', 'sass', 'copy-assets', 'copy-files'
     );
 });
