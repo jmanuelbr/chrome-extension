89b0a030d402e04c440f784983012030a8a6a8c3
Jose Berciano <jmanuelbr@gmail.com>
2019-05-10 07:43:36 +0100

    Integrates Chartist with app to depict temperature/rain/wind graphs 😧😧

--- src/js/components/weather/weather-today.component.jsx
+++ src/js/components/weather/weather-today.component.jsx
@@ -1,72 +1,151 @@
 import React, { Component } from 'react';
-import WeatherTodayItem from './weather-today-item.component';
+import ChartistGraph from 'react-chartist';
+import Chartist from 'chartist';
 
 export default class WeatherToday extends Component {
     constructor(props) {
         super(props);
-        
+        this.state = {
+            selectedOption: 'temperature'
+        };
+        this.handleOptionChange = this.handleOptionChange.bind(this);
     }
     componentDidMount() {
-        this.state = {
-            canvasCtx: document.getElementById("temperature-graph").getContext("2d")
-        };
-        this.drawDots();
+
     }
 
-    drawDots() {
-        // console.log('Drawing dot');
-        const todayArray = this.props.todayData.data;
-        let offset = 3;
-        todayArray.map((hourData, key) => {
-            // console.log('hourData', hourData);
-            this.state.canvasCtx.fillRect(offset,0,1,1);
-            offset +=7;
-        });
+    onDrawHandler(data) {
+        if (data.type === 'point') {
+            let fillColour = 'black';
+            switch (data.seriesIndex) {
+                case 0: {
+                    fillColour = 'blue';
+                    break;
+                }
+                case 1: {
+                    fillColour = '#F05A50';
+                    break;
+                }
+                case 2: {
+                    fillColour = '#F4C63E';
+                    break;
+                }
+                default: {
+                    // Do nothing
+                    break;
+                }
+            }
+            var point = new Chartist.Svg('circle', {
+              cx: data.x,
+              cy: data.y,
+              r:  4,
+              fill: fillColour
+            }, 'ct-slice');
         
+            data.element.replace(point);
+        }
     }
+
+    handleOptionChange(changeEvent) {
+        this.setState({
+          selectedOption: changeEvent.target.value
+        });
+      };
+
     render() {
+        Array.prototype.updateNullDays = function(hoursAdded) {
+            for (var i = 0; i < hoursAdded-1; i++) {
+                this[i] = null;
+            }
+          };
+
+        let hoursList = []; // X axe labels
+        let temperatureList = []; // Y axe labels
+        let allTemperatureList = []; // Useful to calculate max and min temperature for Y axe
         const todayArray = this.props.todayData.data;
-        return(
+        let seriesList =[]; // Array of arrays for all given days
+        let numHoursAdded = 0;
+        let nullDays = [];
+        const todayArrayLength = todayArray.length -1 ;
+        todayArray.map((hourData, key) => {
+            const hour = new Date(hourData.time*1000).getHours();
+            temperatureList.push(hourData.temperature);
+            hoursList.push(hour + "h");
+            allTemperatureList.push(hourData.temperature);
+            numHoursAdded++;
+            if (hour == 0 || key == todayArrayLength) {
+                // break each day into 1 section to depict them in different colors
+                seriesList.push(nullDays.concat(temperatureList));
+                nullDays.updateNullDays(numHoursAdded);
+                const lastElement = temperatureList[temperatureList.length - 1];
+                temperatureList = [];
+                temperatureList.push(lastElement);
+            }
+        });
+ 
+        var data = {
+            labels: hoursList,
+            series: seriesList
+          };
+
+          const maxTemperature = Math.round(Math.max(...allTemperatureList)) + 1;
+          const minTemperature = Math.round(Math.min(...allTemperatureList));
+
+          var options = {
+            high: maxTemperature,
+            low: (minTemperature < 0 ? minTemperature : 0),
+            axisX: {
+              labelInterpolationFnc: function(value, index) {
+                return index % 2 === 0 ? value : null;
+              }
+            },
+            showArea: true,
+            showPoint: true,
+            chartPadding: {
+                left: -10
+              }
+          };
+       
+          var type = 'Line';
+       
+          return (
             <div className="today-hours-panel">
-                {/* {todayArray.map((todayData, key) =>
-                    <WeatherTodayItem
-                        todayData={todayData}
-                        key={key}
-                    />
-                )} */}
-                <canvas id="temperature-graph" width="400" height="57"></canvas>
-                <div className="hours">
-                    {
-                    todayArray.map((hourData, key) => {
-                        const hour = new Date(hourData.time*1000).getHours();
-                        // this.drawDot(hourData);
-                        return (
-                            <div className="hour" key={key}>
-                                {hour}<span className="hour-symbol">"</span>
-                            </div>
-                        )}
-                    )}
+                <div className="select-option">
+                    <div className="radio">
+                        <label>
+                            <input type="radio" value="temperature" 
+                                checked={this.state.selectedOption === 'temperature'} 
+                                onChange={this.handleOptionChange} />
+                            Temperature
+                        </label>
+                    </div>
+                    <div className="radio">
+                        <label>
+                            <input type="radio" value="rain" 
+                                checked={this.state.selectedOption === 'rain'} 
+                                onChange={this.handleOptionChange} />
+                            Rain
+                        </label>
+                    </div>
+                    <div className="radio">
+                        <label>
+                            <input type="radio" value="wind" 
+                                checked={this.state.selectedOption === 'wind'} 
+                                onChange={this.handleOptionChange} />
+                            Wind
+                        </label>
+                    </div>
                 </div>
-                <div className="temperature-meter">
-                    <div className="temperature"> 
-                        <span className="number">40</span>
-                        <span className="celsius">°</span>
-                    </div>
-                    <div className="temperature">
-                        <span className="number">30</span>
-                        <span className="celsius">°</span>
-                    </div>
-                    <div className="temperature">
-                        <span className="number">20</span>
-                        <span className="celsius">°</span>
-                    </div>
-                    <div className="temperature">
-                        <span className="number">10</span>
-                        <span className="celsius">°</span>
-                    </div>                                                                                
+                <div className="temperatures">
+                    <ChartistGraph 
+                        data={data} 
+                        options={options} 
+                        type={type} 
+                        listener={{
+                            draw: e => this.onDrawHandler(e)
+                        }}/>
                 </div>
-                
             </div>
-        );
+          )
     }
 }
