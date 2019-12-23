26e13e2331ca8168b792f7b66c74aff192c28a37
Jose <jose.berciano@cognifide.com>
2019-12-23 14:24:39 +0100

    Code refactoring & cleanup

--- src/js/components/article.component.jsx
+++ src/js/components/article.component.jsx
@@ -6,24 +6,30 @@ export default class Article extends Component {
     }
 
     render() {
-        const {title, link, thumbnail} = this.props.articleData;
+        const {description, link, thumbnail} = this.props.articleData;
+        let {title} = this.props.articleData;
         let cssThumbnail = "";
         if (this.props.css) { // Tailor made CSS for article
             cssThumbnail = this.props.css.cssThumbnail;
         }
+        title += "<span class='toggle-description'> ➡️</span>";
 
         return (
-            <div className="article">
-                <a href={link} target="_blank">
-                    <div className={`thumbnail-container ${cssThumbnail}`}>
-                        <img src={thumbnail}/>
-                    </div>
-                    <p 
-                        className="title"
+            <React.Fragment>
+                {/* <div className="description"
+                    dangerouslySetInnerHTML={{ __html: description }}>
+                </div> */}
+                <div className="article">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                        <div className={`thumbnail-container ${cssThumbnail}`}>
+                            <img src={thumbnail}/>
+                        </div>
+                    </a>
+                    <p className="title"
                         dangerouslySetInnerHTML={{ __html: title }}>
                     </p>
-                </a>
-            </div>
+                </div>
+            </React.Fragment>
         );
     }    
 }
